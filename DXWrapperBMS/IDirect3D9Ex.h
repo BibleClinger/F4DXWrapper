d83bc531a9af5cd5d132936216547e3f52e05434
B.C <58237926+BibleClinger@users.noreply.github.com>
2020-04-12 05:45:44 -0400

    Public inherit from IDirect3D, deleted duplicate code. Suppressed warning regarding enums vs enum classes.

--- DXWrapperBMS/IDirect3D9Ex.h
+++ DXWrapperBMS/IDirect3D9Ex.h
@@ -9,29 +9,13 @@ namespace D9Wrapper
 	class IDirect3D9Ex : public IDirect3D9
 	{
 	protected:
-		D9Real::IDirect3D9Ex* m_IDirect3D9Ex;
+		D9Real::IDirect3D9Ex *m_IDirect3D9Ex;
 	public:
-		IDirect3D9Ex(D9Real::IDirect3D9Ex* obj);
-		/*** IUnknown methods ***/
-		STDMETHOD(QueryInterface)(THIS_ REFIID riid, void** ppvObj);
-		STDMETHOD_(ULONG, AddRef)(THIS);
-		STDMETHOD_(ULONG, Release)(THIS);
+		IDirect3D9Ex(D9Real::IDirect3D9Ex* direct3d) : IDirect3D9(direct3d)
+		{
+			m_IDirect3D9Ex = direct3d; // This points to the same object as inherited member m_IDirect3D9.
+		}
 
-		/*** IDirect3D9 methods ***/
-		STDMETHOD(RegisterSoftwareDevice)(THIS_ void* pInitializeFunction);
-		STDMETHOD_(UINT, GetAdapterCount)(THIS);
-		STDMETHOD(GetAdapterIdentifier)(THIS_ UINT Adapter, DWORD Flags, D9Real::D3DADAPTER_IDENTIFIER9* pIdentifier);
-		STDMETHOD_(UINT, GetAdapterModeCount)(THIS_ UINT Adapter, D9Real::D3DFORMAT Format);
-		STDMETHOD(EnumAdapterModes)(THIS_ UINT Adapter, D9Real::D3DFORMAT Format, UINT Mode, D9Real::D3DDISPLAYMODE* pMode);
-		STDMETHOD(GetAdapterDisplayMode)(THIS_ UINT Adapter, D9Real::D3DDISPLAYMODE* pMode);
-		STDMETHOD(CheckDeviceType)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DevType, D9Real::D3DFORMAT AdapterFormat, D9Real::D3DFORMAT BackBufferFormat, BOOL bWindowed);
-		STDMETHOD(CheckDeviceFormat)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT AdapterFormat, DWORD Usage, D9Real::D3DRESOURCETYPE RType, D9Real::D3DFORMAT CheckFormat);
-		STDMETHOD(CheckDeviceMultiSampleType)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT SurfaceFormat, BOOL Windowed, D9Real::D3DMULTISAMPLE_TYPE MultiSampleType, DWORD* pQualityLevels);
-		STDMETHOD(CheckDepthStencilMatch)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT AdapterFormat, D9Real::D3DFORMAT RenderTargetFormat, D9Real::D3DFORMAT DepthStencilFormat);
-		STDMETHOD(CheckDeviceFormatConversion)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT SourceFormat, D9Real::D3DFORMAT TargetFormat);
-		STDMETHOD(GetDeviceCaps)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DCAPS9* pCaps);
-		STDMETHOD_(HMONITOR, GetAdapterMonitor)(THIS_ UINT Adapter);
-		STDMETHOD(CreateDevice)(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D9Real::D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface);
 		STDMETHOD_(UINT, GetAdapterModeCountEx)(THIS_ UINT Adapter, CONST D9Real::D3DDISPLAYMODEFILTER* pFilter);
 		STDMETHOD(EnumAdapterModesEx)(THIS_ UINT Adapter, CONST D9Real::D3DDISPLAYMODEFILTER* pFilter, UINT Mode, D9Real::D3DDISPLAYMODEEX* pMode);
 		STDMETHOD(GetAdapterDisplayModeEx)(THIS_ UINT Adapter, D9Real::D3DDISPLAYMODEEX* pMode, D9Real::D3DDISPLAYROTATION* pRotation);
--- DXWrapperBMS/IDirect3DEx.cpp
+++ DXWrapperBMS/IDirect3DEx.cpp
@@ -1,94 +1,11 @@
 #include "IDirect3D9Ex.h"
 #include "IDirect3DDevice9Ex.h"
 
+#pragma warning(push)
+#pragma warning(disable : 26812) // Temporarily disable warnings over enums not being enum classes
+
 namespace D9Wrapper
 {
-	IDirect3D9Ex::IDirect3D9Ex(D9Real::IDirect3D9Ex* obj) : IDirect3D9(obj)
-	{
-		m_IDirect3D9Ex = obj;
-	}
-
-	// IUnknown methods
-	STDMETHODIMP IDirect3D9Ex::QueryInterface(THIS_ REFIID riid, void** ppvObj)
-	{
-		return m_IDirect3D9Ex->QueryInterface(riid, ppvObj);
-	}
-	STDMETHODIMP_(ULONG) IDirect3D9Ex::AddRef(THIS)
-	{
-		return m_IDirect3D9Ex->AddRef();
-	}
-	STDMETHODIMP_(ULONG) IDirect3D9Ex::Release(THIS)
-	{
-		return m_IDirect3D9Ex->Release();
-	}
-
-	/*** IDirect3D9 methods ***/
-
-	STDMETHODIMP IDirect3D9Ex::RegisterSoftwareDevice(THIS_ void* pInitializeFunction)
-	{
-		return m_IDirect3D9Ex->RegisterSoftwareDevice(pInitializeFunction);
-	}
-	STDMETHODIMP_(UINT) IDirect3D9Ex::GetAdapterCount(THIS)
-	{
-		return m_IDirect3D9Ex->GetAdapterCount();
-	}
-	STDMETHODIMP IDirect3D9Ex::GetAdapterIdentifier(THIS_ UINT Adapter, DWORD Flags, D9Real::D3DADAPTER_IDENTIFIER9* pIdentifier)
-	{
-		return m_IDirect3D9Ex->GetAdapterIdentifier(Adapter, Flags, pIdentifier);
-	}
-	STDMETHODIMP_(UINT) IDirect3D9Ex::GetAdapterModeCount(THIS_ UINT Adapter, D9Real::D3DFORMAT Format)
-	{
-		return m_IDirect3D9Ex->GetAdapterModeCount(Adapter, Format);
-	}
-	STDMETHODIMP IDirect3D9Ex::EnumAdapterModes(THIS_ UINT Adapter, D9Real::D3DFORMAT Format, UINT Mode, D9Real::D3DDISPLAYMODE* pMode)
-	{
-		return m_IDirect3D9Ex->EnumAdapterModes(Adapter, Format, Mode, pMode);
-	}
-	STDMETHODIMP IDirect3D9Ex::GetAdapterDisplayMode(THIS_ UINT Adapter, D9Real::D3DDISPLAYMODE* pMode)
-	{
-		return m_IDirect3D9Ex->GetAdapterDisplayMode(Adapter, pMode);
-	}
-	STDMETHODIMP IDirect3D9Ex::CheckDeviceType(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DevType, D9Real::D3DFORMAT AdapterFormat, D9Real::D3DFORMAT BackBufferFormat, BOOL bWindowed)
-	{
-		return m_IDirect3D9Ex->CheckDeviceType(Adapter, DevType, AdapterFormat, BackBufferFormat, bWindowed);
-	}
-	STDMETHODIMP IDirect3D9Ex::CheckDeviceFormat(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT AdapterFormat, DWORD Usage, D9Real::D3DRESOURCETYPE RType, D9Real::D3DFORMAT CheckFormat)
-	{
-		return m_IDirect3D9Ex->CheckDeviceFormat(Adapter, DeviceType, AdapterFormat, Usage, RType, CheckFormat);
-	}
-	STDMETHODIMP IDirect3D9Ex::CheckDeviceMultiSampleType(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT SurfaceFormat, BOOL Windowed, D9Real::D3DMULTISAMPLE_TYPE MultiSampleType, DWORD* pQualityLevels)
-	{
-		return m_IDirect3D9Ex->CheckDeviceMultiSampleType(Adapter, DeviceType, SurfaceFormat, Windowed, MultiSampleType, pQualityLevels);
-	}
-	STDMETHODIMP IDirect3D9Ex::CheckDepthStencilMatch(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT AdapterFormat, D9Real::D3DFORMAT RenderTargetFormat, D9Real::D3DFORMAT DepthStencilFormat)
-	{
-		return m_IDirect3D9Ex->CheckDepthStencilMatch(Adapter, DeviceType, AdapterFormat, RenderTargetFormat, DepthStencilFormat);
-	}
-	STDMETHODIMP IDirect3D9Ex::CheckDeviceFormatConversion(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DFORMAT SourceFormat, D9Real::D3DFORMAT TargetFormat)
-	{
-		return m_IDirect3D9Ex->CheckDeviceFormatConversion(Adapter, DeviceType, SourceFormat, TargetFormat);
-	}
-	STDMETHODIMP IDirect3D9Ex::GetDeviceCaps(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, D9Real::D3DCAPS9* pCaps)
-	{
-		return m_IDirect3D9Ex->GetDeviceCaps(Adapter, DeviceType, pCaps);
-	}
-	STDMETHODIMP_(HMONITOR) IDirect3D9Ex::GetAdapterMonitor(THIS_ UINT Adapter)
-	{
-		return m_IDirect3D9Ex->GetAdapterMonitor(Adapter);
-	}
-	STDMETHODIMP IDirect3D9Ex::CreateDevice(THIS_ UINT Adapter, D9Real::D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D9Real::D3DPRESENT_PARAMETERS* pPresentationParameters, IDirect3DDevice9** ppReturnedDeviceInterface)
-	{
-		HRESULT hr;
-		D9Real::IDirect3DDevice9* real_ppReturnedDeviceInterface;
-
-		hr = m_IDirect3D9Ex->CreateDevice(Adapter, DeviceType, hFocusWindow, BehaviorFlags, pPresentationParameters, &real_ppReturnedDeviceInterface);
-		if (hr == D3D_OK)
-		{
-			*ppReturnedDeviceInterface = new IDirect3DDevice9(real_ppReturnedDeviceInterface, this);
-		}
-		else *ppReturnedDeviceInterface = nullptr;
-		return hr;
-	}
 	STDMETHODIMP_(UINT) IDirect3D9Ex::GetAdapterModeCountEx(THIS_ UINT Adapter, CONST D9Real::D3DDISPLAYMODEFILTER* pFilter)
 	{
 		return m_IDirect3D9Ex->GetAdapterModeCountEx(Adapter, pFilter);
@@ -119,3 +36,5 @@ namespace D9Wrapper
 		return m_IDirect3D9Ex->GetAdapterLUID(Adapter, pLUID);
 	}
 }
+
+#pragma warning(pop)
