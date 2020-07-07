79054c1c1e5ee2cd5b7ed931f2e6c84e4f40db17
B.C <58237926+BibleClinger@users.noreply.github.com>
2020-07-07 18:32:38 +0000

    Initial creation of d3d11_main.h. It's based on d3d9_main.h and will have to be updated.

---
+++ F4D11Wrapper/d3d11_main.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "F4DX_common.h"
+
+namespace D11Real
+{
+#include <d3d11.h>
+}
+/*
+namespace D9Wrapper
+{
+	class IDirect3D9;
+	class IDirect3D9Ex;
+	class IDirect3DDevice9;
+	class IDirect3DDevice9Ex;
+}
+*/
+// Typedefs
+
+//typedef D9Real::IDirect3D9* (WINAPI* fnDirect3DCreate9)(UINT);
+//typedef HRESULT(WINAPI* fnDirect3DCreate9Ex)(UINT, D9Real::IDirect3D9Ex**);
+
+// DX9 Hook function
+
+//D9Wrapper::IDirect3D9* WINAPI Direct3DCreate9(UINT SDKVersion);
+//HRESULT WINAPI Direct3DCreate9Ex(UINT SDKVersion, D9Wrapper::IDirect3D9Ex**);
