378314a6d63b1bb10daacc3859b64585acdd66d2
B.C <58237926+BibleClinger@users.noreply.github.com>
2020-07-10 11:57:55 +0000

    Preliminary implementation of the DX11 version. Note: This edition does not try to follow a sequence from begin->end. It just attempts to disable drawing alone.

--- F4D11Wrapper/d3d11_entry.cpp
+++ F4D11Wrapper/d3d11_entry.cpp
@@ -1,4 +1,5 @@
 #include "d3d11_main.h"
+#include "ID3D11DeviceContext.h" // This header needs to be specifically included, because we create an instance with new.
 
 // All entry points should be defined in this file.
 
@@ -21,11 +22,12 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 	return FALSE;
 }
 
-HRESULT D3D11CreateDevice(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,	UINT FeatureLevels, UINT SDKVersion, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Real::ID3D11DeviceContext** ppImmediateContext)
+HRESULT D3D11CreateDevice(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,	UINT FeatureLevels, UINT SDKVersion, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Wrapper::ID3D11DeviceContext** ppImmediateContext)
 {
 #pragma F4DX_EXPORT
 	fnD3D11CreateDevice real_D3D11CreateDevice = nullptr;
 	HRESULT hr;
+	D11Real::ID3D11DeviceContext *ppRealImmediateContext;
 
 	real_D3D11CreateDevice = reinterpret_cast<fnD3D11CreateDevice>(F4DX_manager::getManager().loadFunction(szRealDLLPath, "D3D11CreateDevice"));
 	if (!real_D3D11CreateDevice)
@@ -34,21 +36,23 @@ HRESULT D3D11CreateDevice(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_T
 		*ppImmediateContext = nullptr;
 		return E_FAIL; // For now, we send E_FAIL, because we can't even find the real d3d11.dll. Something is wrong.
 	}
-	hr = real_D3D11CreateDevice(pAdapter, DriverType, Software, Flags, pFeatureLevels, FeatureLevels, SDKVersion, ppDevice, pFeatureLevel, ppImmediateContext);
+	hr = real_D3D11CreateDevice(pAdapter, DriverType, Software, Flags, pFeatureLevels, FeatureLevels, SDKVersion, ppDevice, pFeatureLevel, &ppRealImmediateContext);
 	if (hr == S_OK)
 	{
-		// TODO: This is probably where we want to hook.
+		// This is where we want to hook.
+		*ppImmediateContext = new D11Wrapper::ID3D11DeviceContext(ppRealImmediateContext);
 	}
 	return hr;
 }
 
-HRESULT D3D11CreateDeviceAndSwapChain(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,	UINT FeatureLevels, UINT SDKVersion, const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, D11Real::IDXGISwapChain** ppSwapChain, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Real::ID3D11DeviceContext** ppImmediateContext)
+HRESULT D3D11CreateDeviceAndSwapChain(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,	UINT FeatureLevels, UINT SDKVersion, const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, D11Real::IDXGISwapChain** ppSwapChain, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Wrapper::ID3D11DeviceContext** ppImmediateContext)
 {
 #pragma F4DX_EXPORT
 	fnD3D11CreateDeviceAndSwapChain real_D3D11CreateDeviceAndSwapChain = nullptr;
 	HRESULT hr;
+	D11Real::ID3D11DeviceContext* ppRealImmediateContext;
 
-	real_D3D11CreateDeviceAndSwapChain = reinterpret_cast<fnD3D11CreateDeviceAndSwapChain>(F4DX_manager::getManager().loadFunction(szRealDLLPath, "D3D11CreateDevice"));
+	real_D3D11CreateDeviceAndSwapChain = reinterpret_cast<fnD3D11CreateDeviceAndSwapChain>(F4DX_manager::getManager().loadFunction(szRealDLLPath, "D3D11CreateDeviceAndSwapChain"));
 	if (!real_D3D11CreateDeviceAndSwapChain)
 	{
 		*ppSwapChain = nullptr;
@@ -56,10 +60,11 @@ HRESULT D3D11CreateDeviceAndSwapChain(D11Real::IDXGIAdapter* pAdapter, D11Real::
 		*ppImmediateContext = nullptr;
 		return E_FAIL; // For now, we send E_FAIL, because we can't even find the real d3d11.dll. Something is wrong.
 	}
-	hr = real_D3D11CreateDeviceAndSwapChain(pAdapter, DriverType, Software, Flags, pFeatureLevels, FeatureLevels, SDKVersion, pSwapChainDesc, ppSwapChain, ppDevice, pFeatureLevel, ppImmediateContext);
+	hr = real_D3D11CreateDeviceAndSwapChain(pAdapter, DriverType, Software, Flags, pFeatureLevels, FeatureLevels, SDKVersion, pSwapChainDesc, ppSwapChain, ppDevice, pFeatureLevel, &ppRealImmediateContext);
 	if (hr == S_OK)
 	{
-		// TODO: This is probably where we want to hook.
+		// This is where we want to hook.
+		*ppImmediateContext = new D11Wrapper::ID3D11DeviceContext(ppRealImmediateContext);
 	}
 	return hr;
 }
--- F4D11Wrapper/d3d11_main.h
+++ F4D11Wrapper/d3d11_main.h
@@ -8,72 +8,19 @@ namespace D11Real
 {
 #include <d3d11.h>
 }
-/*
-namespace D9Wrapper
+
+namespace D11Wrapper
 {
-	class IDirect3D9;
-	class IDirect3D9Ex;
-	class IDirect3DDevice9;
-	class IDirect3DDevice9Ex;
+	class ID3D11DeviceContext;
 }
-*/
+
+
 // Typedefs
 
-//typedef D9Real::IDirect3D9* (WINAPI* fnDirect3DCreate9)(UINT);
-//typedef HRESULT(WINAPI* fnDirect3DCreate9Ex)(UINT, D9Real::IDirect3D9Ex**);
+typedef HRESULT (WINAPI * fnD3D11CreateDevice)(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Real::ID3D11DeviceContext** ppImmediateContext);
+typedef HRESULT (WINAPI * fnD3D11CreateDeviceAndSwapChain)(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, D11Real::IDXGISwapChain** ppSwapChain, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Real::ID3D11DeviceContext** ppImmediateContext);
 
-typedef HRESULT(WINAPI* fnD3D11CreateDevice)(D11Real::IDXGIAdapter* pAdapter,
-	D11Real::D3D_DRIVER_TYPE         DriverType,
-	HMODULE                 Software,
-	UINT                    Flags,
-	const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,
-	UINT                    FeatureLevels,
-	UINT                    SDKVersion,
-	D11Real::ID3D11Device** ppDevice,
-	D11Real::D3D_FEATURE_LEVEL* pFeatureLevel,
-	D11Real::ID3D11DeviceContext** ppImmediateContext);
+// DX11 Hook functions
 
-typedef HRESULT (WINAPI * fnD3D11CreateDeviceAndSwapChain)(
-	D11Real::IDXGIAdapter* pAdapter,
-	D11Real::D3D_DRIVER_TYPE            DriverType,
-	HMODULE                    Software,
-	UINT                       Flags,
-	const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,
-	UINT                       FeatureLevels,
-	UINT                       SDKVersion,
-	const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc,
-	D11Real::IDXGISwapChain** ppSwapChain,
-	D11Real::ID3D11Device** ppDevice,
-	D11Real::D3D_FEATURE_LEVEL* pFeatureLevel,
-	D11Real::ID3D11DeviceContext** ppImmediateContext
-);
-
-// DX9 Hook function
-
-HRESULT D3D11CreateDevice(
-	D11Real::IDXGIAdapter* pAdapter,
-	D11Real::D3D_DRIVER_TYPE         DriverType,
-	HMODULE                 Software,
-	UINT                    Flags,
-	const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,
-	UINT                    FeatureLevels,
-	UINT                    SDKVersion,
-	D11Real::ID3D11Device** ppDevice,
-	D11Real::D3D_FEATURE_LEVEL* pFeatureLevel,
-	D11Real::ID3D11DeviceContext** ppImmediateContext
-);
-
-HRESULT D3D11CreateDeviceAndSwapChain(
-	D11Real::IDXGIAdapter* pAdapter,
-	D11Real::D3D_DRIVER_TYPE            DriverType,
-	HMODULE                    Software,
-	UINT                       Flags,
-	const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels,
-	UINT                       FeatureLevels,
-	UINT                       SDKVersion,
-	const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc,
-	D11Real::IDXGISwapChain** ppSwapChain,
-	D11Real::ID3D11Device** ppDevice,
-	D11Real::D3D_FEATURE_LEVEL* pFeatureLevel,
-	D11Real::ID3D11DeviceContext** ppImmediateContext
-);
+HRESULT D3D11CreateDevice(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Wrapper::ID3D11DeviceContext** ppImmediateContext);
+HRESULT D3D11CreateDeviceAndSwapChain(D11Real::IDXGIAdapter* pAdapter, D11Real::D3D_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, const D11Real::D3D_FEATURE_LEVEL* pFeatureLevels, UINT FeatureLevels, UINT SDKVersion, const D11Real::DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, D11Real::IDXGISwapChain** ppSwapChain, D11Real::ID3D11Device** ppDevice, D11Real::D3D_FEATURE_LEVEL* pFeatureLevel, D11Wrapper::ID3D11DeviceContext** ppImmediateContext);
